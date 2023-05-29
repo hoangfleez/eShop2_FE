6cc534c4134aaf967d899fa12aade98225f1ae15
quang <ltb01628645546@gmail.com>
2023-05-29 20:13:37 +0700

    chinh sua giao dien trang home

---
+++ src/sevives/productService.js
@@ -0,0 +1,11 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import customAPI from "./customAPI";
+
+export const getProduct = createAsyncThunk(
+    'products/getProducts',
+    async () => {
+        const res = await customAPI.get('products');
+        return res.data;
+    }
+)
+
